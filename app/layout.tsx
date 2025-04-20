6e7f8963c66caf7148722fc670703c38123696e5
Tapasya <mailrip00@gmail.com>
2025-04-20 22:26:23 +0530

    Added Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
+import Navbar from "@/components/main/Navbar";
 
 // import { Footer } from "@/components/main/footer";
 // import { Navbar } from "@/components/main/navbar";
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         }
       >
         <StarsCanvas />
+        <Navbar/>
         {children}
       </body>
     </html>
