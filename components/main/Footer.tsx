d6a056eebc9ba9be443d5499218a5b5f0c605034
Tapasya <mailrip00@gmail.com>
2025-04-28 00:06:18 +0530

    finalized footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -7,7 +7,9 @@ import {
   RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
-import { FaStackOverflow } from "react-icons/fa";
+import { FaStackOverflow, FaTelegramPlane } from "react-icons/fa"; 
+import { BsEnvelope, BsCalendar2Check } from "react-icons/bs"; 
+
 
 const Footer = () => {
   return (
@@ -17,96 +19,127 @@ const Footer = () => {
           
           {/* Community */}
           <div className="min-w-[200px] flex flex-col items-center">
-            <div className="font-bold text-[16px] mb-2">Community</div>
+            <div className="relative z-50 w-full bg-transparent text-gray-200 p-4">
+              <div className="flex flex-col items-center">
+                 <div className="font-bold text-lg">Community</div>
 
-            <a
-              href="https://stackoverflow.com/users/16920486/tapasya-baidya"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center my-2 transition-all hover:text-cyan-400"
-            >
-              <FaStackOverflow />
-              <span className="ml-2 text-[15px]">StackOverflow</span>
-            </a>
+                  <a
+                    href="https://github.com/knownstranger-Tapasya/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center my-2 transition-all hover:text-cyan-400"
+                  >
+                    <RxGithubLogo />
+                    <span className="text-[15px] ml-[6px]">Github</span>
+                  </a>
 
-            <a
-              href="https://github.com/knownstranger-Tapasya/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center my-2 transition-all hover:text-cyan-400"
-            >
-              <RxGithubLogo />
-              <span className="ml-2 text-[15px]">Github</span>
-            </a>
+                  <a
+                    href="https://discord.com/channels/knownstranger1662"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center my-2 transition-all hover:text-cyan-400"
+                  >
+                    <RxDiscordLogo />
+                    <span className="text-[15px] ml-[6px]">Discord</span>
+                  </a>
+                  
+                  <a
+                    href="https://stackoverflow.com/users/16920486/tapasya-baidya"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center my-2 transition-all hover:text-cyan-400"
+                  >
+                    <FaStackOverflow />
+                    <span className="ml-2 text-sm">StackOverflow</span>
+                  </a>
 
-            <a
-              href="https://discord.com/channels/knownstranger1662"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center my-2 transition-all hover:text-cyan-400"
-            >
-              <RxDiscordLogo />
-              <span className="ml-2 text-[15px]">Discord</span>
-            </a>
+                </div>
+              </div>
+        </div>
+
+        {/* Social Media */}
+        <div className="min-w-[200px] flex flex-col items-center">
+          <div className="relative z-50 w-full bg-transparent text-gray-200 p-4">
+            <div className="flex flex-col items-center">
+              <div className="font-bold text-lg">Social Media</div>
+
+              <a
+                href="https://x.com/0KnownStranger0"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center my-2 hover:text-cyan-400 transition"
+              >
+                <RxTwitterLogo />
+                <span className="text-[15px] ml-[6px]">Twitter</span>
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/tapasya-baidya/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center my-2 hover:text-cyan-400 transition"
+              >
+                <RxLinkedinLogo />
+                <span className="text-[15px] ml-[6px]">Linkedin</span>
+              </a>
+              
+              <a
+                href="https://www.instagram.com/_knownstranger_/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center my-2 hover:text-cyan-400 transition"
+              >
+                <RxInstagramLogo />
+                <span className="ml-2 text-sm">Instagram</span>
+              </a>
+
+            </div>
           </div>
+        </div>
 
-          {/* Social Media */}
+          {/* Contact */}
           <div className="min-w-[200px] flex flex-col items-center">
             <div className="relative z-50 w-full bg-transparent text-gray-200 p-4">
               <div className="flex flex-col items-center">
-               <div className="font-bold text-lg">Social Media</div>
-
+                <div className="font-bold text-lg mb-2">Contact</div>
                 <a
-                  href="https://www.instagram.com/_knownstranger_/"
+                  href="mailto:tapasyabaidya@gmail.com?subject=Let's%20Connect&body=Hi%20Tapasya%2C%20I%20checked%20out%20your%20portfolio%20and..."
+                  className="flex items-center my-2 hover:text-cyan-400 cursor-pointer transition-all"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="w-full flex items-center justify-center mt-2 hover:text-cyan-400 transition"
                 >
-                  <RxInstagramLogo />
-                  <span className="ml-2 text-sm">Instagram</span></a>
-
-                <a
-                  href="https://x.com/0KnownStranger0"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="w-full flex items-center justify-center mt-2 hover:text-cyan-400 transition"
-                >
-                  <RxTwitterLogo />
-                  <span className="text-[15px] ml-[6px]">Twitter</span>
+                  <BsEnvelope />
+                  <span className="ml-2 text-sm">Reach Out</span>
                 </a>
 
                 <a
-                  href="https://www.linkedin.com/in/tapasya-baidya/"
+                  href="https://calendly.com/mailrip00"
+                  className="flex items-center my-2 hover:text-cyan-400 cursor-pointer transition-all"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="w-full flex items-center justify-center mt-2 hover:text-cyan-400 transition"
                 >
-                  <RxLinkedinLogo />
-                  <span className="text-[15px] ml-[6px]">Linkedin</span>
+                  <BsCalendar2Check />
+                  <span className="ml-2 text-sm">Book a Meeting</span>
                 </a>
+
+                <a
+                  href="https://t.me/KNOWNSTRANGER1"
+                  className="flex items-center my-2 hover:text-cyan-400 cursor-pointer transition-all"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaTelegramPlane />
+                  <span className="ml-2 text-sm">Message on Telegram</span>
+                </a>
+
+               
+                </div>
               </div>
             </div>
           </div>
 
-
-          {/* Contact */}
-          <div className="min-w-[200px] flex flex-col items-center">
-            <div className="font-bold text-[16px] mb-2">Contact</div>
-
-            <p className="my-2 hover:text-cyan-400 cursor-pointer">About</p>
-            <p className="my-2 hover:text-cyan-400 cursor-pointer">Learning About Me</p>
-
-            <a
-              href="mailto:tapasyabaidya@gmail.com?subject=Let's%20Connect&body=Hi%20Tapasya%2C%20I%20checked%20out%20your%20portfolio%20and..."
-              className="my-2 hover:text-cyan-400 cursor-pointer"
-            >
-              Reach Out
-            </a>
-          </div>
-        </div>
-
         <div className="mt-8 text-center text-[14px] text-gray-400">
-          ©2025 Tapasya&apos;s Portfolio Hub. All Rights Reserved.
+         ©2025 Tapasya&apos;s Portfolio Hub. All Rights Reserved.
         </div>
       </div>
     </div>
